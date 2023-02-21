8bc41c6c8fcc933e201a5e25cf982aa9b66eaec4
c4rl0s01 <barahona5carlos@gmail.com>
2023-02-21 18:01:27 +0100

    reading data for ej2

--- main.py
+++ main.py
@@ -2,6 +2,31 @@ import pandas as pd
 import sqlite3
 
 
+def rellenar():
+
+    c = sqlite3.connect('example.db') #connect to db
+    data = pd.read_csv('alerts.csv')  #read data from csv
+
+    data.to_sql('example.db', c, if_exists='replace', index=False) #add data to db
+
+    c.commit() #commit changes
+    c.close()  #close connection to db
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    rellenar()
+
+
+
+
 
 
 
