6e1a56f314ed17705810e2b8dcda58c2452c5eb0
c4rl0s01 <barahona5carlos@gmail.com>
2023-02-21 18:04:05 +0100

    db removed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def rellenar():
     c = sqlite3.connect('example.db') #connect to db
     data = pd.read_csv('alerts.csv')  #read data from csv
 
-    data.to_sql('example.db', c, if_exists='replace', index=False) #add data to db
+    data.to_sql('Alertas', c, if_exists='replace', index=False) #add data to db
 
     c.commit() #commit changes
     c.close()  #close connection to db
