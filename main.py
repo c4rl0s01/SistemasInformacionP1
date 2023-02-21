df743b6f7c80ab4a3c25387d8807f3423aff8f4c
c4rl0s01 <barahona5carlos@gmail.com>
2023-02-21 20:51:09 +0100

    el pandas no lee el json

--- main.py
+++ main.py
@@ -5,25 +5,30 @@ import sqlite3
 def rellenar():
 
     c = sqlite3.connect('example.db') #connect to db
-    data = pd.read_csv('alerts.csv')  #read data from csv
 
-    data.to_sql('Alertas', c, if_exists='replace', index=False) #add data to db
+    data = pd.read_csv('alerts.csv')  #read data from csv
+    data.to_sql('Alerts', c, if_exists='replace', index=False) #add data to db
 
     c.commit() #commit changes
     c.close()  #close connection to db
 
 
 
+def show_db():
+    c = sqlite3.connect('example.db')
+    cursor = c.cursor()
 
+    cursor.execute('SELECT * FROM Alerts')
+    rows = cursor.fetchall()
 
-
-
-
-
+    for row in rows:
+            print(row)
+    cursor.close()
+    c.close()
 
 if __name__ == '__main__':
     rellenar()
-
+    #show_db()
 
 
 
